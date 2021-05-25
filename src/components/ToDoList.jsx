0cceb58d80e03724d9986b1983c2a16fd09a19bb
fabricio cunha <fabriciocunhadeveloper@gmail.com>
2021-05-25 04:13:19 +0100

    Creating todos

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './to-do-list.css';
 
 function ToDoList() {
+    const [toDoContent, setToDoContent] = useState('');
+
+    // function handleChange(event) {
+    //     setToDoContent(event);
+    // }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const toDos = document.querySelector('.to-dos-lists');
+        toDos.innerHTML = `<li>${toDoContent}</li>`;
+
+        const toDoInput = document.querySelector('#to-do');
+        toDoInput.value = "";
+    }
+
     return (
         <div className="to-do-container">
-            <form>
-                <label htmlFor="to-do">TODO</label>
-                <input type="text" name="to-do" id="to-do" required />
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="to-do">
+                    TODO
+                    <input
+                        type="text"
+                        name="to-do"
+                        id="to-do"
+                        required
+                        onChange={(e) => setToDoContent(e.target.value)}
+                    />
+                </label>
             </form>
 
             <div className="to-dos">
-                <ul className="to-dos-lists">
-                    <li>To Do</li>
-                    <li>To Do</li>
-                    <li>To Do</li>
-                </ul>
+                <ul className="to-dos-lists"></ul>
                 <div className="to-do-options">
                     <p>0 items left</p>
                     <ul>
